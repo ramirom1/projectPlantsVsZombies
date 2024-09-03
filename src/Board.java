4de19a86cc104d331dc9bf9aab5a7225ae2c7557
Ramiro <ramiromartinez050@gmail.com>
2024-09-02 22:23:05 -0300

    modifique board

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,27 @@
 //es na matriz que tiene almacenada las plantas, zombies y proyectiles
+import Entity.Entity;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private final int COL = 10, FILAS = 5;
+    private boolean existsRepetidora, existsGirasol, existsPatatapum;
+    public List<Entity>[][] board;
+
+    public Board(){
+        this.existsRepetidora = false;
+        this.existsGirasol = false;
+        this.existsPatatapum = false;
+        board = new List[5][10];
+    }
+
+    public void move(int oldX, int oldY, int newX, int newY){
+
+    }
+
+    public void printBoard(){
+
+    }
 }
