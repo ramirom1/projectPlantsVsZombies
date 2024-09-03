ca7d70fe8b35a278fb7ea033744a85608e431ae9
Ramiro <ramiromartinez050@gmail.com>
2024-09-02 22:32:12 -0300

    Actualizacion

--- src/zombie/Zombie.java
+++ src/zombie/Zombie.java
@@ -1,11 +1,26 @@
 package zombie;
 import Entity.Entity;
+import Entity.Attack;
 
-public class Zombie extends Entity {
-    private int vidaZombie;
+public class Zombie extends Entity implements Attack {
+    private int speed;
 
-    public Zombie(int vidaZombie, int life, String name, int positionX, int positionY) {
-        super(life, name, positionX, positionY);
-        this.vidaZombie = vidaZombie;
+    public Zombie(int positionY) {
+        super(500, "Zombie", 10, positionY);
+        this.speed = 1; //1 velocidad normal, 2 velocidad rápida
     }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public void attack(){
+    }
+
+    //El juego va a encargarse de mover de lugar a los zombies
 }
